e77661ee3d10f62bcc68869b76c0ffce4ecd730e
Anna Andrade <anna.1504@outlook.com>
2025-06-12 17:24:51 -0300

    tipos-de-dados

---
+++ index.js
@@ -0,0 +1,15 @@
+const nome = "Anna";
+const idade = 25;
+const estudante = true;
+const hobbies = ["Ler", "Treinar", "Escrever", "Andar de bicicleta"];
+const endereco = { rua: "Ângela", cidade: "Rio de Janeiro"};
+
+console.log (typeof nome);
+console.log (typeof idade);
+console.log (typeof estudante);
+console.log (typeof hobbies);
+console.log (typeof endereco);
+
+console.log ("Oi, me chamo " + nome + " e tenho " + idade + " anos.")
+
+console.log ("Oi, me chamo " + nome.toUpperCase() + " e tenho " + idade + " anos.")
